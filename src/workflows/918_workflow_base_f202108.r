ca23583a5b5f96c941f8e470279fef30d5502dad
stevilca <65248803+stevilca@users.noreply.github.com>
2024-11-21 13:03:55 -0300

    Update 918_workflow_base_f202108.r
    Cambio del nombre del dataset

--- src/workflows/918_workflow_base_f202108.r
+++ src/workflows/918_workflow_base_f202108.r
@@ -425,8 +425,9 @@ wf_agosto <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea workflow inicial fija
 
   # Etapa especificacion dataset de la Segunda Competencia Kaggle
-  DT_incorporar_dataset( "~/buckets/b1/datasets/competencia_02.csv.gz")
-
+  #DT_incorporar_dataset( "~/buckets/b1/datasets/competencia_02.csv.gz")
+  #cambio 21/11/24
+  DT_incorporar_dataset( "~/buckets/b1/datasets/competencia02_FS.csv.gz")
   # Etapas preprocesamiento
   CA_catastrophe_base( metodo="MachineLearning")
   FEintra_manual_base()
