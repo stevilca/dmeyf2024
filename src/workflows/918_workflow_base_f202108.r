3655b1e2ac280d0e497f40257afd2d1778a49978
stevilca <65248803+stevilca@users.noreply.github.com>
2024-11-23 16:39:12 -0300

    Update 918_workflow_base_f202108.r
    agrege un comentario

--- src/workflows/918_workflow_base_f202108.r
+++ src/workflows/918_workflow_base_f202108.r
@@ -280,7 +280,7 @@ TS_strategy_base8 <- function( pinputexps )
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
   #param_local$final_train$training <- c(202106, 202105, 202104,
-    202103, 202102, 202101)
+    #202103, 202102, 202101)
   #23/11/24 SE AGREGAN 24 MESES
   param_local$final_train$training <- c(202106, 202105, 202104,
     202103, 202102, 202101, 202012, 202011, 202010, 202009, 202008,
