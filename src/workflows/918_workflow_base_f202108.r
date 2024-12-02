e51661339df3310a5d7f6054ab4a8550bf5640e5
stevilca <65248803+stevilca@users.noreply.github.com>
2024-12-02 11:15:31 -0300

    Update 918_workflow_base_f202108.r

--- src/workflows/918_workflow_base_f202108.r
+++ src/workflows/918_workflow_base_f202108.r
@@ -442,8 +442,8 @@ KA_evaluate_kaggle <- function( pinputexps )
 {
   if( -1 == (param_local <- exp_init())$resultado ) return( 0 )# linea fija
 
- # param_local$meta$script <- "/src/wf-etapas/z2603_KA_evaluate_kaggle.r"
-  param_local$meta$script <- "/src/wf-etapas/z2603_KA_evaluate_kaggle_SEMI.r"
+  param_local$meta$script <- "/src/wf-etapas/z2601_KA_evaluate_kaggle.r"
+  #param_local$meta$script <- "/src/wf-etapas/z2603_KA_evaluate_kaggle_SEMI.r"
 
 
   param_local$semilla <- NULL  # no usa semilla, es deterministico
